91b1f18a2a6dd7f5720296613e367ebdd2ea1145
SHase964 <52561425+SHase964@users.noreply.github.com>
2023-06-05 12:27:59 +0900

    Add files via upload

---
+++ Data/01181519_1pl_400_10_5000/irts/IRT_icl_cmd.tcl
@@ -0,0 +1,9 @@
+output -log_file irt_log.iclo
+set_default_model_dichtomous 1PL
+options -D 1.7
+allocate_items_dist 33 -num_latent_dist_points 20
+read_examinees ../skills/skill_9_train1.dat 33i1
+starting_values_dichotomous
+EM_steps -max_iter 2000
+write_item_param irt_param_skill_9_train1.iclp
+release_items_dist
